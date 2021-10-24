12353fdea30364126e0c7c6ef996934fea1f6804
Yegor <yegor.l@mail.ru>
2021-10-24 20:57:03 +0300

    Removed some tests

--- Calculator/CalculatorClassLibraryTests/CalculatorTests.cs
+++ Calculator/CalculatorClassLibraryTests/CalculatorTests.cs
@@ -5,35 +5,35 @@ namespace CalculatorClassLibrary.Tests
 {
     public class CalculatorTests
     {
-        // SUM TESTS
-        [Fact]
-        public void ItShould_sum10_and5_returns15() => Assert.Equal(15, Calculator.Sum(10, 5));
+        //// SUM TESTS
+        //[Fact]
+        //public void ItShould_sum10_and5_returns15() => Assert.Equal(15, Calculator.Sum(10, 5));
 
-        [Fact]
-        public void ItShould_sumSizeOfInt_and1_throwsException() => Assert.Throws<Exception>(
-            () => Calculator.Sum(int.MaxValue, int.MaxValue));
+        //[Fact]
+        //public void ItShould_sumSizeOfInt_and1_throwsException() => Assert.Throws<Exception>(
+        //    () => Calculator.Sum(int.MaxValue, int.MaxValue));
 
-        [Fact]
-        public void ItShould_sumMinus10_and10_returns0() => Assert.Equal(0, Calculator.Sum(-10, 10));
+        //[Fact]
+        //public void ItShould_sumMinus10_and10_returns0() => Assert.Equal(0, Calculator.Sum(-10, 10));
 
 
-        // DIVIDE TESTS
-        [Fact]
-        public void ItShould_divide100_by10_returns10() => Assert.Equal(10, Calculator.Divide(100, 10));
+        //// DIVIDE TESTS
+        //[Fact]
+        //public void ItShould_divide100_by10_returns10() => Assert.Equal(10, Calculator.Divide(100, 10));
 
-        [Fact]
-        public void ItShould_divide1_by0_throwsDivideByZeroException() =>
-            Assert.Throws<DivideByZeroException>(() =>  Calculator.Divide(1, 0));
+        //[Fact]
+        //public void ItShould_divide1_by0_throwsDivideByZeroException() =>
+        //    Assert.Throws<DivideByZeroException>(() =>  Calculator.Divide(1, 0));
 
-        [Fact]
-        public void ItShould_divide1_byMinus1_returnsMinus1() => Assert.Equal(-1, Calculator.Divide(1, -1));
+        //[Fact]
+        //public void ItShould_divide1_byMinus1_returnsMinus1() => Assert.Equal(-1, Calculator.Divide(1, -1));
 
-        // SUBSTRACT TESTS
-        [Fact]
-        public void ItShould_substract0_and5_returnsMinus5() => Assert.Equal(-5, Calculator.Substract(0, 5));
+        //// SUBSTRACT TESTS
+        //[Fact]
+        //public void ItShould_substract0_and5_returnsMinus5() => Assert.Equal(-5, Calculator.Substract(0, 5));
 
-        [Fact]
-        public void ItShould_substractMinus5_andMinus5_returns0() => Assert.Equal(0, Calculator.Substract(-5, -5));
+        //[Fact]
+        //public void ItShould_substractMinus5_andMinus5_returns0() => Assert.Equal(0, Calculator.Substract(-5, -5));
 
         // MULTIPLY TESTS
         [Fact]
