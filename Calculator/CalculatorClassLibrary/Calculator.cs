53827878c031db665684e6f62a18461783f7a9cc
Yegor <yegor.l@mail.ru>
2021-10-24 17:26:07 +0300

    Added some unit tests

---
+++ Calculator/CalculatorClassLibrary/Calculator.cs
@@ -0,0 +1,13 @@
+﻿namespace CalculatorClassLibrary
+{
+    public static class Calculator
+    {
+        public static int Sum(int a, int b) => a + b;
+
+        public static int Substract(int a, int b) => a - b;
+
+        public static int Multiply(int a, int b) => a * b;
+
+        public static int Divide(int a, int b) => a / b;
+    }
+}
